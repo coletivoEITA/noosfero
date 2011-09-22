38fb0c7bda061f096d27515d5fbdf6c9eddb3dfb
Braulio Bhavamitra <brauliobo@gmail.com>
2011-09-22 11:02:27 -0300

    Fix ordering of options

--- plugins/former/models/former_plugin_option_field.rb
+++ plugins/former/models/former_plugin_option_field.rb
@@ -1,3 +1,3 @@
 class FormerPluginOptionField < FormerPluginField
-  has_many :options, :class_name => 'FormerPluginOption', :foreign_key => :field_id, :dependent => :destroy
+  has_many :options, :class_name => 'FormerPluginOption', :foreign_key => :field_id, :order => 'id asc', :dependent => :destroy
 end
