21f41fc410fc5d8711e1ff6b77851ec428b59830
Braulio Bhavamitra <brauliobo@gmail.com>
2011-10-07 16:05:01 -0300

    Bugfixes

--- plugins/sniffer/lib/sniffer_plugin.rb
+++ plugins/sniffer/lib/sniffer_plugin.rb
@@ -25,7 +25,8 @@ class SnifferPlugin < Noosfero::Plugin
   def self.ext
     Product.named_scope :suppliers_products, lambda { |enterprise|
       km_lat, km_lng = 111.2, 85.3
-      { :select => "DISTINCT products_2.id, products_2.name, products.id as my_product_id, products.name as my_product_name,
+      {
+      :select => "DISTINCT products_2.id, products_2.name, products.id as my_product_id, products.name as my_product_name,
         profiles.id as profile_id, profiles.identifier as profile_identifier, profiles.name as profile_name, profiles.lat as profile_lat, profiles.lng as profile_lng,
         inputs.product_category_id, categories.name as product_category_name,
         'supplier_product' as view,
@@ -34,12 +35,15 @@ class SnifferPlugin < Noosfero::Plugin
         INNER JOIN categories ON ( inputs.product_category_id = categories.id )
         INNER JOIN products products_2 ON ( categories.id = products_2.product_category_id )
         INNER JOIN profiles ON ( profiles.id = products_2.enterprise_id )",
-      :conditions => "(products.enterprise_id = #{enterprise.id} )" }
+      :conditions => "products.enterprise_id = #{enterprise.id}
+        AND profiles.public_profile = true AND profiles.visible = true AND profiles.validated = true"
+      }
     }
 
     Product.named_scope :buyers_products, lambda { |enterprise|
       km_lat, km_lng = 111.2, 85.3
-      { :select => "DISTINCT products.id, products.name, products_2.id as my_product_id, products_2.name as my_product_name,
+      {
+      :select => "DISTINCT products.id, products.name, products_2.id as my_product_id, products_2.name as my_product_name,
         profiles.id as profile_id, profiles.identifier as profile_identifier, profiles.name as profile_name, profiles.lat as profile_lat, profiles.lng as profile_lng,
         inputs.product_category_id, categories.name as product_category_name,
         'buyer_product' as view,
@@ -48,12 +52,15 @@ class SnifferPlugin < Noosfero::Plugin
         INNER JOIN categories ON ( inputs.product_category_id = categories.id )
         INNER JOIN products products_2 ON ( categories.id = products_2.product_category_id )
         INNER JOIN profiles ON ( profiles.id = products.enterprise_id )",
-      :conditions => "products_2.enterprise_id = #{enterprise.id}" }
+      :conditions => "products_2.enterprise_id = #{enterprise.id}
+        AND profiles.public_profile = true AND profiles.visible = true AND profiles.validated = true"
+      }
     }
 
     Product.named_scope :interests_suppliers_products, lambda { |profile|
       km_lat, km_lng = 111.2, 85.3
-      { :from => "sniffer_plugin_profiles sniffer",
+      {
+      :from => "sniffer_plugin_profiles sniffer",
       :select => "DISTINCT products.id, products.name,
         profiles.id as profile_id, profiles.identifier as profile_identifier, profiles.name as profile_name, profiles.lat as profile_lat, profiles.lng as profile_lng,
         categories.id as product_category_id, categories.name as product_category_name,
@@ -63,12 +70,15 @@ class SnifferPlugin < Noosfero::Plugin
         INNER JOIN categories categories ON ( op.opportunity_id = categories.id )
         INNER JOIN products ON ( products.product_category_id = categories.id )
         INNER JOIN profiles ON ( products.enterprise_id = profiles.id )",
-      :conditions => "sniffer.enabled = true AND sniffer.profile_id = #{profile.id} AND products.enterprise_id <> #{profile.id}" }
+      :conditions => "sniffer.enabled = true AND sniffer.profile_id = #{profile.id} AND products.enterprise_id <> #{profile.id}
+        AND profiles.public_profile = true AND profiles.visible = true AND profiles.validated = true"
+      }
     }
 
     Product.named_scope :interests_buyers_products, lambda { |profile|
       km_lat, km_lng = 111.2, 85.3
-      { :select => "DISTINCT products.id, products.name,
+      {
+      :select => "DISTINCT products.id, products.name,
         profiles.id as profile_id, profiles.identifier as profile_identifier, profiles.name as profile_name, profiles.lat as profile_lat, profiles.lng as profile_lng,
         categories.id as product_category_id, categories.name as product_category_name,
         'interest_buyer_product' as view,
@@ -77,7 +87,9 @@ class SnifferPlugin < Noosfero::Plugin
         INNER JOIN sniffer_plugin_opportunities as op ON ( categories.id = op.opportunity_id AND op.opportunity_type = 'ProductCategory' )
         INNER JOIN sniffer_plugin_profiles sniffer ON ( op.profile_id = sniffer.id AND sniffer.enabled = true )
         INNER JOIN profiles ON ( sniffer.profile_id = profiles.id )",
-      :conditions => "products.enterprise_id = #{profile.id} AND profiles.id <> #{profile.id}" }
+      :conditions => "products.enterprise_id = #{profile.id} AND profiles.id <> #{profile.id}
+        AND profiles.public_profile = true AND profiles.visible = true AND profiles.validated = true"
+      }
     }
 
     Enterprise.has_many :input_categories, :through => :inputs, :source => :product_category, :uniq => true
