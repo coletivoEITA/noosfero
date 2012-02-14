59199d73157201c1eaca6c81fc663fa3db101d66
Alan Freihof Tygel <alantygel@gmail.com>
2012-02-14 17:55:31 -0200

    Correction of column names: evaluation -> object

--- plugins/evaluation/models/evaluation_plugin/evaluation.rb
+++ plugins/evaluation/models/evaluation_plugin/evaluation.rb
@@ -1,9 +1,8 @@
 class EvaluationPlugin::Evaluation < Noosfero::Plugin::ActiveRecord
-  belongs_to :evaluation, :polymorphic => true
+  belongs_to :object, :polymorphic => true
   belongs_to :evaluator, :class_name => "Profile"
   belongs_to :evaluated, :class_name => "Profile"
  
-  validates_presence_of :evaluation_type
-  validates_presence_of :evaluation_id
+  validates_presence_of :object
 
 end
