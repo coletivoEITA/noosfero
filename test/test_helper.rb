f4cf179e8dad552d970f91e3a2c44187ab79a9f9
Braulio Bhavamitra <brauliobo@gmail.com>
2012-04-14 20:47:35 +0000

    [Rails3] Using tidy_ffi instead of tidy

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -126,13 +126,11 @@ class ActiveSupport::TestCase
     else
       get action, params
     end
-    tidy = Tidy.open(:show_warnings=>false)
-    tidy.options.output_xml = true
-    tidy.clean @response.body
+    tidy = TidyFFI::Tidy.new(@response.body, :output_xml => 1)
+    tidy.clean
     if tidy.errors
       flunk "HTML ERROR - Tidy Diagnostics:\n  "+
-            tidy.errors.join("\n  ") +"\n  "+
-            tidy.diagnostics.join("\n  ")
+            tidy.errors.join("\n  ")
     end
   end
 
