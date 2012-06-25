25f20a03a4f6456992b0132bf3e02bd54542be52
Braulio Bhavamitra <brauliobo@gmail.com>
2012-06-25 18:24:08 +0000

    Fix require_dependency on product extension

--- plugins/exchange/lib/ext/product.rb
+++ plugins/exchange/lib/ext/product.rb
@@ -1,4 +1,4 @@
-require 'product'
+require_dependency '../../../../app/models/product'
 
 class Product
   has_many :elements, :foreign_key => "element_id", :conditions => {:element_type => "Product"}, :class_name => 'ExchangePlugin::ExchangeElement', :dependent => :destroy
