9e9a8cea8c2e3e937168e92883973587e7690a5b
Braulio Bhavamitra <brauliobo@gmail.com>
2012-07-11 22:14:44 +0000

    Add inputs to interests block

--- plugins/sniffer/lib/sniffer_plugin/interests_block.rb
+++ plugins/sniffer/lib/sniffer_plugin/interests_block.rb
@@ -21,8 +21,9 @@ class SnifferPlugin::InterestsBlock < Block
     lambda do
       sniffer = SnifferPluginProfile.find_or_create(block.owner)
       interests = sniffer.opportunities
+      inputs = sniffer.profile.enterprise? ? sniffer.profile.inputs : nil
       render :file => 'blocks/sniffer_plugin/interests_block',
-        :locals => {:block => block, :interests => interests}
+        :locals => {:block => block, :interests => interests, :inputs => inputs}
     end
   end
 
