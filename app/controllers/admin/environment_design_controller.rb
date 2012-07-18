eb175ce6286f47d57c072f190e3363b48df71587
Braulio Bhavamitra <brauliobo@gmail.com>
2012-07-18 22:26:47 +0000

    Add support for environment block made in plugins

--- app/controllers/admin/environment_design_controller.rb
+++ app/controllers/admin/environment_design_controller.rb
@@ -1,9 +1,13 @@
 class EnvironmentDesignController < BoxOrganizerController
-  
+
   protect 'edit_environment_design', :environment
+
+  protected
 
   def available_blocks
     @available_blocks ||= [ ArticleBlock, LoginBlock, EnvironmentStatisticsBlock, RecentDocumentsBlock, EnterprisesBlock, CommunitiesBlock, PeopleBlock, SellersSearchBlock, LinkListBlock, FeedReaderBlock, SlideshowBlock, HighlightsBlock, FeaturedProductsBlock, CategoriesBlock, RawHTMLBlock ]
+
+    @available_blocks += @plugins.dispatch(:environment_blocks, environment) if @plugins
   end
 
 end
