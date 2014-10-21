coletivoEITA/noosfero-ecosol

OAuth client and provider plugin

plugins/oauth/lib/ext/songkick/o_auth2/model/client.rb
@@ -1,0 +1,22 @@
+require_dependency 'songkick/o_auth2/model/client'
+
+class Songkick::OAuth2::Model::Client
+
+  attr_accessible :site, :image_builder
+
+  validates_presence_of :site
+
+  acts_as_having_image
+  after_update :save_image
+
+  def image_public_filename size='icon'
+    self.image.public_filename size if self.image
+  end
+
+  protected
+
+  def save_image
+    image.save if image
+  end
+
+end

coletivoEITA/noosfero

plugins/oauth/lib/ext/songkick/o_auth2/model/client.rb
@@ -1,0 +1,22 @@
+require_dependency 'songkick/oauth2/model/client'
+
+class Songkick::OAuth2::Model::Client
+
+  attr_accessible :site, :image_builder
+
+  validates_presence_of :site
+
+  acts_as_having_image
+  after_update :save_image
+
+  def image_public_filename size='icon'
+    self.image.public_filename size if self.image
+  end
+
+  protected
+
+  def save_image
+    image.save if image
+  end
+
+end